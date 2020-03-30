aac248bac47f809db0ac2e0d687d2048adb26dd7
Pblfer <pblfertec@gmail.com>
2020-03-30 11:51:00 -0600

    Hero Component

---
+++ src/components/LevelSelect/HeroComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="carousel-example">
+        <swiper :options="swiperOption" :dir="$vs.rtl ? 'rtl' : 'ltr'" :key="$vs.rtl">
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-16.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-13.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-4.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-2.jpg" alt="banner">
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.min.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data () {
+    return {
+      swiperOption: {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 4500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
+    }
+  },
+  components:{
+    swiper,
+    swiperSlide
+  }
+}
+</script>
--- src/views/pages/panel/Panel.vue
+++ src/views/pages/panel/Panel.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <p>dash</p>
+        <HeroComponent/>
     </div>
 </template>
 
 <script>
+import HeroComponent from '@/components/LevelSelect/HeroComponent'
 export default {
-        
+  data () {
+    return {
+      colorx:'#1877f2'
+    }
+  },
+  components:{
+    HeroComponent
+  }
 }
 </script>
 
